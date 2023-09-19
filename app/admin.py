0cbd3b1a1dc314390430ecd046bf2615886800a0
= Md. Kawser Ahmed <kawserahmed8492@gmail.com>
2023-09-19 12:39:39 +0600

    model class implemented

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Customer,Product,Cart,OrderedPlaced
 
 # Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['id','user', 'name', 'locality', 'city', 'zipcode', 'state']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id','title', 'selling_price', 'discount_price', 'description', 'brand', 'category','product']
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id','user','product','quantity']
+
+@admin.register(OrderedPlaced)
+class OrderedPlacedAdmin(admin.ModelAdmin):
+    list_display = ['id','user', 'customer', 'product', 'quantity', 'order_date', 'status']
+
+# Every model has a id by default
